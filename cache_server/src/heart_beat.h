811507d231b7d5d3bfe6001bba195343c0e67d2f
icf <2277075501@qq.com>
2022-06-25 22:57:41 +0800

    cacheserver

---
+++ cache_server/src/heart_beat.h
@@ -0,0 +1,15 @@
+#ifndef HEART_BEAT
+#define HEART_BEATif
+
+class HeartBeat{
+
+public:
+    void connectTask();
+
+    void heartBeatSend();
+
+private:
+    
+};
+
+#endif
---
+++ master/build/CMakeFiles/testSafeQuue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testSafeQuue.dir/test/test_safeQueue.cpp.o"
+  "testSafeQuue"
+  "testSafeQuue.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testSafeQuue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
